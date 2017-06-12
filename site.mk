df565eb924dc21c2b8e09ec7536a0a5a9d2fb746
Matthias Fritzsche <matthias@chemnitz.freifunk.net>
2017-06-13 01:40:18 +0200

    site.mk: whitespaces überall gleich einrücken und überall Tabs
    Signed-off-by: Matthias Fritzsche <matthias@chemnitz.freifunk.net>

--- site.mk
+++ site.mk
@@ -1,40 +1,40 @@
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-15 \
-	gluon-alfred \
-	gluon-respondd \
-	gluon-autoupdater \
-	gluon-setup-mode \
-	gluon-config-mode-core \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-authorized-keys \
-	gluon-web-admin \
-    gluon-web-autoupdater \
-    gluon-web-network \
-    gluon-web-wifi-config \
-	gluon-mesh-vpn-fastd \
-	gluon-radvd \
-	gluon-status-page \
-	iwinfo \
-	iptables \
-	haveged
+        gluon-mesh-batman-adv-15 \
+        gluon-alfred \
+        gluon-respondd \
+        gluon-autoupdater \
+        gluon-setup-mode \
+        gluon-config-mode-core \
+        gluon-config-mode-hostname \
+        gluon-config-mode-mesh-vpn \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-contact-info \
+        gluon-ebtables-filter-multicast \
+        gluon-ebtables-filter-ra-dhcp \
+        gluon-authorized-keys \
+        gluon-web-admin \
+        gluon-web-autoupdater \
+        gluon-web-network \
+        gluon-web-wifi-config \
+        gluon-mesh-vpn-fastd \
+        gluon-radvd \
+        gluon-status-page \
+        iwinfo \
+        iptables \
+        haveged
 
 ifeq ($(GLUON_TARGET),x86-generic)
 GLUON_SITE_PACKAGES += \
-    kmod-usb-core \
-    kmod-usb2 \
-    kmod-usb-hid
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid
 endif
 
 ifeq ($(GLUON_TARGET),x86-64)
 GLUON_SITE_PACKAGES += \
-    kmod-usb-core \
-    kmod-usb2 \
-    kmod-usb-hid
+        kmod-usb-core \
+        kmod-usb2 \
+        kmod-usb-hid
 endif
 
 DEFAULT_GLUON_RELEASE := b$(shell date '+%Y%m%d')

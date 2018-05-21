41d08e43a0c5d77fa2c5ef572aafbe531a0fa204
Matthias Fritzsche <matthias@chemnitz.freifunk.net>
2018-05-21 20:37:51 +0200

    site.mk: add gluon-web-node-role
    Signed-off-by: Matthias Fritzsche <matthias@chemnitz.freifunk.net>

--- site.mk
+++ site.mk
@@ -16,6 +16,7 @@ GLUON_SITE_PACKAGES := \
         gluon-web-admin \
         gluon-web-autoupdater \
         gluon-web-network \
+	gluon-web-node-role \
         gluon-web-wifi-config \
         gluon-mesh-vpn-fastd \
         gluon-radvd \

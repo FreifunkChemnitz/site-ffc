94092040c37b5f1c50a3ea18a7aa06b9df92b9b7
Holminateur <holm@mailbox.org>
2018-06-28 08:50:24 +0200

    Workaround for gluon-status-page dependency error
    - somehow the gluon-status-page package does not pull the
    package gluon-status-page-mesh-batman-adv (formerly gluon-status-page-api)
    - as a workaround include it in the site package list

--- site.mk
+++ site.mk
@@ -20,6 +20,7 @@ GLUON_SITE_PACKAGES := \
         gluon-mesh-vpn-fastd \
         gluon-radvd \
         gluon-status-page \
+        gluon-status-page-mesh-batman-adv \
         iwinfo \
         iptables \
         haveged \

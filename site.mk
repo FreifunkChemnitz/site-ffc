3e6a0560596c3470b295072369e79604b0293e73
Matthias Fritzsche <matthias@chemnitz.freifunk.net>
2019-09-26 18:21:43 +0200

    support usb storage & umts except for ar71xx-tiny & ramips-rt305x

--- site.mk
+++ site.mk
@@ -24,18 +24,29 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-ifeq ($(GLUON_TARGET),x86-generic)
-GLUON_SITE_PACKAGES += \
-    kmod-usb-core \
-    kmod-usb2 \
-    kmod-usb-hid
+FLASH_4MB := false
+ifeq ($(GLUON_TARGET),ar71xx-tiny)
+       FLASH_4MB := true
+endif
+ifeq ($(GLUON_TARGET),ramips-rt305x)
+       FLASH_4MB := true
 endif
 
-ifeq ($(GLUON_TARGET),x86-64)
+ifeq ($(FLASH_4MB),false)
 GLUON_SITE_PACKAGES += \
-    kmod-usb-core \
+	comgt \
+	ppp \
+	kmod-fs-ext4 \
+	kmod-nls-utf8 \
     kmod-usb2 \
     kmod-usb-hid
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-serial \
+	kmod-usb-serial-option \
+	kmod-usb-serial-wwan \
+	kmod-usb-storage
 endif
 
 DEFAULT_GLUON_RELEASE := b$(shell date '+%Y%m%d')
